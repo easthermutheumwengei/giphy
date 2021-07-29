471c40dc2fae30ba59c64566698d85d6c78b6a02
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-07-29 10:47:23 +0300

    itial commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  gifs = new BehaviorSubject<any>([]);
+  
+    
+  constructor(private http: HttpClient) { }
+  getTrendingGifs() {
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.apiUrl}`)
+      .subscribe((response: any) => {
+        this.gifs.next(response.data)
+      });
+  }
+  searchGifs(gifName:string) {
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${environment.apiUrl}&q=${gifName}`)
+    .subscribe((response: any) => {
+      this.gifs.next(response.data)
+    });
+  }
+  getGifs() {
+    return this.gifs.asObservable();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl:"CXQSBBEkszz5eijJcq8Dmel2SvtNd94C",
+};
